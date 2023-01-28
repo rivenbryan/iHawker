89d15844cf525e74867f366ef66e94bfb3695455
Aloysius Tan <atan134@e.ntu.edu.sg>
2023-01-28 12:10:19 +0800

    Pushed Server

---
+++ ihawkers/backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import stall_router from "./api/stall/stall.route.js"
+import hawker_router from "./api/hawker/hawker.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/hawker", hawker_router)
+app.use("/api/stall", stall_router)
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
+app.listen(5000, () => console.log("Server Running"))
+
+export default app
