cb6049bd075ea6d9501fb5c7db4a508d28e0f806
AloysiusTan <97999822+AloysiusTan@users.noreply.github.com>
2023-01-31 22:20:18 +0800

    Updated DB

--- backend/api/hawker/hawker.controller.js
+++ backend/api/hawker/hawker.controller.js
@@ -1,32 +1,59 @@
-import hawker from "../../Hawker.json" assert {type:"json"}
+import {hawkerCollection} from "../../index.js"
+import {ObjectId} from "mongodb"
 
-export const getAllHawker=(req, res) => {
+
+export const getAllHawker= async (req, res) => {
+    const hawkers = await hawkerCollection.find({}).toArray()
+    res.status(200).json(hawkers)
+}
+
+export const createHawker= async (req, res) => {
+    const insertedOutput = await hawkerCollection.insertOne(req.body)
+    const insertedHawker = await hawkerCollection.findOne({_id: insertedOutput.insertedId})
+    if (insertedHawker == null) {
+        res.status(500).send("Request failed")
+        return
+    }
+    res.status(201).send(insertedHawker)
+}
+
+export const getHawkerById= async (req, res) => {
+    const {id}=req.params
+    let hawker
+    try {
+        hawker = await hawkerCollection.findOne({_id: new ObjectId(id)})
+    } catch (err) {
+        res.status(400).send("Invlaid ID not found")
+        return
+    }
+    if(hawker == null) {
+        res.status(404).send("Hawker not found")
+        return
+    }
     res.status(200).json(hawker)
 }
 
-export const createHawker=(req, res) => {
-    hawker.push(req.body)
-    res.status(201).send()
-}
 
-export const getHawkerById=(req, res) => {
+
+export const deleteHawkerById= async (req, res) => {
     const {id}=req.params
-    if (id >= hawker.length) {
-        res.status(404).send()
+    let hawker
+    try {
+        hawker = await hawkerCollection.findOne({_id: new ObjectId(id)})
+    }
+    catch (err) {
+        res.status(400).send("Invlaid ID not found")
         return
     }
-    res.status(200).json(hawker[id])
-}
-
-export const deleteHawkerById=(req, res) => {
-    const {id}=req.params
-    if (id >= hawker.length) {
-        res.status(404).send()
+    if(hawker == null) {
+        res.status(404).send("Hawker not found")
         return
     }
-    hawker.remove(id)
+    await hawkerCollection.deleteOne({_id: new ObjectId(id)})
     res.status(200).send()
 }
+
+
 
 const HawkerController={
     getAllHawker,
