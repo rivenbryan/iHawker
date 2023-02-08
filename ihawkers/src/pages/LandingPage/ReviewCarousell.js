d93a3291eb191e82d7362d3f7bd8df6de41fe508
Bryan Tay <rivenbryan@gmail.com>
2023-02-09 00:05:14 +0800

    feat: added reviewCarouselljs

---
+++ ihawkers/src/pages/LandingPage/ReviewCarousell.js
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import Carousel from 'react-material-ui-carousel';
+export default function ReviewCarousell() {
+    
+    return (
+      <>
+      </>
+    );
+}
