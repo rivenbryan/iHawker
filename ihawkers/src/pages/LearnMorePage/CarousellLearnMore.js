658844342e40a615181aedb789fdf2929811fae4
Bryan Tay <rivenbryan@gmail.com>
2023-02-08 23:52:24 +0800

    rmv: remove carousell

--- ihawkers/src/pages/LearnMorePage/CarousellLearnMore.js
+++ ihawkers/src/pages/LearnMorePage/CarousellLearnMore.js
@@ -1,30 +1,24 @@
-import React from 'react'
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Box, Container} from '@mui/material'
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Box, Container } from "@mui/material";
 
 export default function CarousellLearnMore() {
-    
-    var items = [
-        {
-            img: "car4.jpg"
-        },
-        {
-            img: "car3.jpeg"
-        }
-    ]
+  var items = [
+    {
+      img: "car4.jpg",
+    }
+  ];
 
   return (
-    <Box sx={{paddingTop: 5}}>
-        <Carousel animation="slide" indicators={false}>
-                {
-                    items.map( (item, i) =>        
-                    <Container maxWidth="xl" >
-                        <Box sx={{width: "100%"}}component="img" alt="Company Logo"
-                        src={process.env.PUBLIC_URL + "/img/info/" + item.img}>
-                        </Box>
-                    </Container>
-                )}
-        </Carousel>
+    <Box sx={{ paddingTop: 5 }}>
+      <Container maxWidth="xl">
+        <Box
+          sx={{ width: "100%" }}
+          component="img"
+          alt="Company Logo"
+          src={process.env.PUBLIC_URL + "/img/info/" + items[0].img}
+        ></Box>
+      </Container>
     </Box>
-  )
+  );
 }
