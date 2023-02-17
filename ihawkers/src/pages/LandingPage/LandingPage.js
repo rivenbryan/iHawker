a8905cd8d7f9dfb2f21aa2362586306bd51f1660
Bryan <rivenbryan@gmail.com>
2023-02-17 20:07:17 +0800

    added chatbot

--- ihawkers/src/pages/LandingPage/LandingPage.js
+++ ihawkers/src/pages/LandingPage/LandingPage.js
@@ -6,19 +6,19 @@ import Footer from '../../components/Footer'
 import React from "react";
 import Fade from 'react-reveal/Fade';
 import ReviewCarousell from './ReviewCarousell'
+import Chatbot from './Chatbot'
 export default function LandingPage() {
-  
-
   return (
     <>
-        <Navbar />
-        <Fade bottom>
-          <Hero/>
-          <Search/>
-          <ReviewCarousell/>
-          <Carousell/>
-        </Fade>
-        <Footer/>
+      <Chatbot/>
+      <Navbar />
+      <Fade bottom>
+        <Hero />
+        <Search />
+        <ReviewCarousell />
+        <Carousell />
+      </Fade>
+      <Footer />
     </>
   )
 }
