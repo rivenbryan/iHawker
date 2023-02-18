293de15939bd892fb2dc16f1ed27099809108197
digudiguding <98702501+digudiguding@users.noreply.github.com>
2023-02-18 18:03:52 +0800

    Integrate with backend for StallPage

--- ihawkers/src/pages/StallPage/StallPage.js
+++ ihawkers/src/pages/StallPage/StallPage.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import StallDesc from './StallDesc'
 import StallMenu from './StallMenu'
 import ViewReviews from './ViewRecentReview'
 import AddReview from './AddReview'
+import { HawkerContext } from '../../context/HawkerContext'
 
 export default function StallPage() {
+
+  const { oneHawkerStall } = useContext(HawkerContext);
+  const { hawkerStores } = useContext(HawkerContext);
+
   return (
     <>
         <Navbar/>
