1f8b2caf24b75f5d4b9c5f74d939197b3224a811
Bryan <rivenbryan@gmail.com>
2023-02-20 13:38:04 +0800

    added more features for chat

--- ihawkers/src/pages/LandingPage/Chatbot.js
+++ ihawkers/src/pages/LandingPage/Chatbot.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
-import { config, steps, theme } from '../../components/Chatbot'
+import { config, steps, theme } from '../LandingPage/Chatbot/Chatbot'
 export default function Chatbot() {
     return (
         <ThemeProvider theme={theme}>
