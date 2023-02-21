a58078e7f2fd75fe63e086da1e390277194befba
AloysiusTan <97999822+AloysiusTan@users.noreply.github.com>
2023-02-21 21:59:15 +0800

    Working

--- ihawkers/src/pages/ForgetPasswordPage/ForgetPasswordAfterEmail.js
+++ ihawkers/src/pages/ForgetPasswordPage/ForgetPasswordAfterEmail.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -8,7 +8,38 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Navbar from '../../components/Navbar';
+import {useLocation} from "react-router-dom";
 export default function ForgetPasswordAfterEmail() {
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const token = queryParams.get('token')
+  const [flag, setFlag] = useState(false)
+  const [error,setError] = useState("")
+  const handleSubmit = (e) => {e.preventDefault()
+    const form = e.currentTarget
+    if (form.elements.password.value != form.elements.confirmPassword.value) {
+      setFlag(true)
+      return
+    }
+    fetch('http://localhost:4000/api/auth/reset-password', {
+      method: "POST",
+      body: JSON.stringify({
+        "newPassword": form.elements.confirmPassword.value,
+        token
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(async (response) => {
+      if (response.ok) {
+        //redirect to Landing Page
+        window.location.href = "/"
+      }
+      else {
+        const errorMessage = await response.json()
+        console.log(errorMessage)
+        setError(errorMessage)
+      }
+    })}
+
   return (
 
     <>
@@ -29,32 +60,28 @@ export default function ForgetPasswordAfterEmail() {
         <Typography component="h1" variant="h5">
           Reset Password
         </Typography>
-        <Box component="form" noValidate onSubmit={{}} sx={{ mt: 3 }}>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={12}>
               <TextField
                 autoComplete="given-name"
-
                 required
                 fullWidth
-                id="name"
+                id="password"
                 label="Password"
                 autoFocus
                 // value={{}}
-                onChange={{}}
               />
             </Grid>
             <Grid item xs={12} sm={12}>
               <TextField
                 autoComplete="given-name"
-
                 required
                 fullWidth
-                id="name"
+                id="confirmPassword"
                 label="Confirm Password"
                 autoFocus
                 // value={{}}
-                onChange={{}}
               />
             </Grid>
     
@@ -68,6 +95,8 @@ export default function ForgetPasswordAfterEmail() {
           </Button>
           </Grid>
         </Box>
+        {flag ? <Box>Password do not match. Try again</Box> : null}
+        {error ? <Box>{error}</Box> : null}
       </Container>
     </Container>
     </>
