e5887c4615e84a5a01dd6019bef4a08d245384cd
Aloysius Tan <97999822+AloysiusTan@users.noreply.github.com>
2023-02-27 22:21:24 +0800

    Updated with the cookies

--- backendv2/models/user.model.js
+++ backendv2/models/user.model.js
@@ -60,6 +60,7 @@ User.statics.signup = async function(userInput){
 }
 
 User.statics.getUser = async function(token) {
+    console.log(token)
     if (!token) return null
     const tokenInfo = await jwt.verify(token, process.env.SECRET)
     const user = await this.findById(tokenInfo.id)
@@ -70,6 +71,7 @@ User.statics.getUser = async function(token) {
 //checkHawker = True when checking for Hawker Privilege Otherwise is False
 User.statics.checkUserType = async function(token, checkHawker) {
     const user = await this.getUser(token)
+    console.log(user)
     //I want to give Hawker Privilege
     if (checkHawker) {
         //return True if user exist + user is Hawker
