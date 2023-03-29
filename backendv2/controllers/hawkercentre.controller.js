0f92f1864b69f1b9d4f7e8503e7411c3b020b539
AloysiusTan <97999822+AloysiusTan@users.noreply.github.com>
2023-03-29 14:17:21 +0800

    Update hawkercentre.controller.js

--- backendv2/controllers/hawkercentre.controller.js
+++ backendv2/controllers/hawkercentre.controller.js
@@ -4,16 +4,43 @@ const UserModel = require("../models/user.model")
 const StallModel = require("../models/stall.model")
 const mongoose = require("mongoose")
 
+/**
+ * Get all Hawker Centres sorted by number of stalls.
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - List of all Hawker Centres sorted by number of stalls in descending order.
+ */
+
 const getAllHawkercentre= async (req, res) => {
     const hawkercentres = await HawkercentreModel.find().sort({no_of_stalls: -1})
     res.status(200).json(hawkercentres)
 }
+
+/**
+ * Get all Stalls for a Hawker Centre by ID.
+ * @function
+ * @async
+ * @param {Object} req - Express request object containing the ID of the Hawker Centre.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - List of all stalls for the specified Hawker Centre.
+ */
 
 const getAllStalls = async (req, res) => {
     const {id} = req.params
     const stalls = await StallModel.find({hawker_centre_belong: mongoose.Types.ObjectId(id)})
     res.status(200).json(stalls)
 }
+
+/**
+ * Create a new Hawker Centre.
+ * @function
+ * @async
+ * @param {Object} req - Express request object containing the details of the new Hawker Centre.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - The newly created Hawker Centre.
+ */
 
 const createHawkercentre= async (req, res) => {
     const { name_of_centre, location_of_centre, no_of_stalls} = req.body
@@ -32,6 +59,15 @@ const createHawkercentre= async (req, res) => {
     res.cookie("token", token).status(201).json(hawkercentre)
 }
 
+/**
+ * Get a specific Hawker Centre by ID.
+ * @function
+ * @async
+ * @param {Object} req - Express request object containing the ID of the Hawker Centre.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - The Hawker Centre with the specified ID.
+ */
+
 const getHawkercentreById = async (req, res) => {
     const {id}=req.params
     let hawkercentre
@@ -46,6 +82,13 @@ const getHawkercentreById = async (req, res) => {
     }
     res.status(200).json(hawkercentre)
 }
+
+/**
+ * Deletes a specific hawker centre by its ID.
+ * @param {Object} req - The HTTP request object
+ * @param {Object} res - The HTTP response object
+ * @returns {Object} - The status of the delete operation
+ */
 
 const deleteHawkercentreById= async (req, res) => {
     const {id}=req.params
@@ -69,11 +112,23 @@ const deleteHawkercentreById= async (req, res) => {
     res.cookie("token", token).status(200).send()
 }
 
+/**
+ * Updates a hawker centre record by ID
+ *
+ * @async
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {Object} - Returns a JSON object with the updated hawker centre data
+ * @throws {Error} - Returns a status 401 if user is not authorized. Returns a status 400 if invalid ID is provided.
+ * Returns a status 404 if hawker centre is not found. Returns a status 500 if an error occurs while updating the hawker centre.
+ */
+
 const updateHawkercentre = async (req,res) => {
     const {id} = req.params
     const {name_of_centre, location_of_centre, no_of_stalls , img} = req.body
     const {token} = req.cookies
-    
+
     //Check for Hawker Privilege
     const userType = await UserModel.checkUserType(token, true)
     if (!userType) {
@@ -95,6 +150,16 @@ const updateHawkercentre = async (req,res) => {
     await hawkercentre.save()
     res.cookie("token", token).status(200).send(hawkercentre)
 }
+
+/**
+ * An object containing functions that handle HTTP requests related to hawker centres.
+ * @typedef {Object} HawkercentreController
+ * @property {function} getAllHawkercentre - A function that retrieves all hawker centres.
+ * @property {function} getAllStalls - A function that retrieves all the stalls by the hawker centre ID.
+ * @property {function} createHawkercentre - A function that create hawker centre.
+ * @property {function} getHawkercentreById - A function that retrieves the hawker centre by ID
+ * @property {function} updateHawkercentre - A function that deletes a single stall by its ID.
+ */
 
 const HawkercentreController = {
     getAllHawkercentre,
